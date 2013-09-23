da839a19e0cf30222547694cdf54a982209e66f5
raydipasupil@gmail.com <raydipasupil@gmail.com>
2013-09-22 22:30:12 -0700

    CRUD Practice

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+	
+	def index
+		@posts = Post.all
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(params[:post].permit(:title, :text))
+
+		if @post.save
+			redirect_to @post
+		else 
+			render 'new'
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :text)
+		end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '20a65034bfb09074445ccec16d6fd385518383080ca2e313a903687f80cc7fbe352ddb20d8f11e395fd45e66a205e2f2a191a976ed910c050c463c7443a9f7d8'
